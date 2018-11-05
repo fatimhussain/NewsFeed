72ee5e4b8adc453079fb8be8f1923f2905145fb6
fatimhussain <fatima.khadimhussain@yahoo.com>
2018-11-05 10:45:55 -0800

    first commit: created the xcode project

---
+++ NewsFeed/NewsFeed/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NewsFeed
+//
+//  Created by Fatima on 11/5/18.
+//  Copyright © 2018 Fatima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
