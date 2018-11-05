fbc647e91cbf714c98265a3ce23a4f66cb196dad
fatimhussain <fatima.khadimhussain@yahoo.com>
2018-11-05 12:14:23 -0800

    created Articlelistview controller and cell to display the Ariticle data

---
+++ NewsFeed/NewsFeed/Cells/ArticleListCell.swift
@@ -0,0 +1,34 @@
+//
+//  ArticleListCell.swift
+//  NewsFeed
+//
+//  Created by Fatima on 11/5/18.
+//  Copyright © 2018 Fatima. All rights reserved.
+//
+
+import UIKit
+
+class ArticleListCell: UITableViewCell {
+
+    @IBOutlet weak var articleImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var publishDateLabel: UILabel!
+    @IBOutlet weak var descriptionCell: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    func configureCell(with article: Article) {
+        titleLabel.text = article.title
+        publishDateLabel.text = article.publishedAt
+        descriptionCell.text = article.desc
+    }
+}
