a7982477b3767c44a44ac612cba7127e9e600360
fatimhussain <fatima.khadimhussain@yahoo.com>
2018-11-05 11:32:13 -0800

    created APIManagerclass to handle the data and services

--- NewsFeed/NewsFeed/Network Layer/APIManager.swift
+++ NewsFeed/NewsFeed/Network Layer/APIManager.swift
@@ -7,3 +7,17 @@
 //
 
 import Foundation
+
+class APIManager: NSObject {
+    
+    static let shared = APIManager()
+    
+    var categories: [Category]? {
+        return [Category(name: "Crypto Coins News", url: "everything?q=bitcoin&from=2018-10-05&sortBy=publishedAt"),
+                Category(name: "The Wall Street Journal", url: "top-headlines?country=us&category=business"),
+                Category(name: "Engadget", url: "everything?q=apple&from=2018-11-04&to=2018-11-04&sortBy=popularity"),
+                Category(name: "TechCrunch", url: "top-headlines?sources=techcrunch"),
+                Category(name: "The Wall Street Journal", url: "everything?domains=wsj.com")
+        ]
+    }
+}
